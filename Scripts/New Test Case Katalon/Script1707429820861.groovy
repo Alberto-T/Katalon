70db0c5fddc521dd9146108733c79a0ac6382d5f
Alberto-T <alberto.torres.cancino@gmail.com>
2024-02-08 19:37:13 -0300

    New TC Katalon was modified

--- Scripts/New Test Case Katalon/Script1707429820861.groovy
+++ Scripts/New Test Case Katalon/Script1707429820861.groovy
@@ -17,3 +17,15 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://demoqa.com/automation-practice-form')
+
+WebUI.maximizeWindow()
+
+WebUI.setText(findTestObject('Object Repository/Page_DEMOQA/inputuserEmail'), 'alberto@gmail.com')
+
+WebUI.setText(findTestObject('Object Repository/Page_DEMOQA/textareacurrentAddress'), 'hola123')
+
+WebUI.closeBrowser()
+
